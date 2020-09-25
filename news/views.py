89564cfe8b06136470add88fd11dc9fb92d6576f
james musyoka <jamesmu475@gmail.com>
2020-09-25 23:27:45 +0300

    We have imported the Http404 function from django.http module

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime as dt
 
 def welcome(request):
@@ -10,10 +10,16 @@ def news_of_day(request):
 
 
 def past_days_news(request,past_date):
+
+    try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-        
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+
     day = convert_dates(date)
     html = f'''
         <html>
